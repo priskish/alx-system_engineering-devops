cbf9c6c00fd49658e7cc13d4d2ea6859293e3447
priskish <101426953+priskish@users.noreply.github.com>
2022-04-12 21:45:21 +0100

    function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - return abs value
+ * @n : number to check
+ * Return:0 or 1
+ */
+
+int _abs(int n)
+{
+
+return (n >= 0 ? n : n * -1);
+
+}
+
+
+7-print_last_digit.c
